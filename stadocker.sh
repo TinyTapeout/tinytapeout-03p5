31bc751fee316b3887c99d098474641446bd11b5
matt venn <matt@mattvenn.net>
2023-06-09 16:10:23 +0200

    sta

---
+++ stadocker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo $OPENLANE_ROOT
+docker run --rm \
+    -e PDK_ROOT=$PDK_ROOT \
+    -v $(pwd):$(pwd) \
+    -v $OPENLANE_ROOT:$OPENLANE_ROOT \
+    -v $PDK_ROOT:$PDK_ROOT \
+    -u $(id -u $USER):$(id -g $USER) \
+    efabless/openlane:a35b64aa200c91e9eb7dde56db787d6b4c0ea12a-amd64 \
+    /bin/bash -c "cd $(pwd) ; sh verilog/gl/sta.sh | tee sta.log"
